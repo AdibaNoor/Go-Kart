9f2606a827f796a8bf11bba816a7ccd2300e9777
Saurrabhh <saurabhkyadav99@gmail.com>
2022-09-18 16:17:49 +0530

    added google maps

---
+++ lib/Pages/map_page.dart
@@ -0,0 +1,45 @@
+import 'dart:async';
+import 'package:flutter/material.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+
+class MapPage extends StatefulWidget {
+  const MapPage({Key? key}) : super(key: key);
+
+  @override
+  State<MapPage> createState() => MapPageState();
+}
+
+class MapPageState extends State<MapPage> {
+  final Completer<GoogleMapController> _controller = Completer();
+
+  static const CameraPosition myHome = CameraPosition(
+    target: LatLng(21.16126705305539, 81.22605545901469),
+    zoom: 18,
+  );
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: GoogleMap(
+        mapType: MapType.hybrid,
+        initialCameraPosition: myHome,
+        markers: {
+          Marker(markerId: const MarkerId("Home"), position: myHome.target)
+        },
+        onMapCreated: (GoogleMapController controller) {
+          _controller.complete(controller);
+        },
+      ),
+      floatingActionButton: FloatingActionButton.extended(
+        onPressed: _goToHome,
+        label: const Text('To Anjora!'),
+        icon: const Icon(Icons.directions_boat),
+      ),
+    );
+  }
+
+  Future<void> _goToHome() async {
+    final GoogleMapController controller = await _controller.future;
+    controller.animateCamera(CameraUpdate.newCameraPosition(myHome));
+  }
+}
