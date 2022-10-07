bea53d81c43a1df8f79b4da52507a12f799b420c
PiyushJoshi0103 <piyush3joshi@gmail.com>
2022-10-08 03:25:27 +0530

    first change Speedometer.dart added charts

--- lib/Pages/Speedometer.dart
+++ lib/Pages/Speedometer.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:syncfusion_flutter_charts/charts.dart';
+import 'package:intl/intl.dart';
 
 class SpeedometerPage extends StatefulWidget  {
   const SpeedometerPage({Key? key}) : super(key: key);
@@ -8,21 +10,52 @@ class SpeedometerPage extends StatefulWidget  {
 }
 
 class _SpeedometerPageState extends State<SpeedometerPage> {
+  late List<SalesData> _chatData;
+  late TooltipBehavior _tooltipBehavior;
+
   @override
-  //yaha se udanahai
-  Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.black,
-      body: Container(
-        child: Column(
-          children: [
-            Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 25.0,vertical: 30),
-              child: Text('SpeedometerPage',style: TextStyle(fontSize: 28,color: Colors.white),),
-            )
-          ],
-        ),
-      ),
-    );
+  void initState(){
+    _chatData = getCharData();
+    _tooltipBehavior = TooltipBehavior(enable:true);
+    super.initState();
   }
+
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(child: Scaffold(body: SfCartesianChart(
+      title: ChartTitle(text:'Speed of Vehical'),
+      legend: Legend(isVisible: true),
+      tooltipBehavior: _tooltipBehavior,
+      series:<ChartSeries>[
+        LineSeries<SalesData,double>(
+          name:'speed',
+          dataSource: _chatData,
+          xValueMapper: (SalesData sales, _) => sales.year,
+          yValueMapper: (SalesData sales ,_) => sales.sales,
+          dataLabelSettings: const DataLabelSettings(isVisible: true),
+        enableTooltip: true)
+      ],
+      primaryYAxis: NumericAxis(edgeLabelPlacement: EdgeLabelPlacement.shift),
+      primaryXAxis: NumericAxis(
+          labelFormat: '{value}M',
+          numberFormat: NumberFormat.simpleCurrency(decimalDigits: 0)),
+    ),
+    ));
+  }
+  List<SalesData>getCharData(){
+    final List<SalesData> chartData=[
+      SalesData(2017,25),
+      SalesData(2018,12),
+      SalesData(2019,24),
+      SalesData(2022,18),
+      SalesData(2021,30),
+    ];
+    return chartData;
+  }
+}
+
+class SalesData{
+  SalesData(this.year,this.sales);
+  final double year;
+  final double sales;
 }
