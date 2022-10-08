3f47bdf13d7fb627627dee9a08e3d1a5fb9335a2
Saurrabhh <saurabhkyadav99@gmail.com>
2022-10-08 20:28:33 +0530

    dynamic change in graph

--- lib/Pages/Speedometer.dart
+++ lib/Pages/Speedometer.dart
@@ -1,8 +1,11 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 import 'package:syncfusion_flutter_charts/charts.dart';
 import 'package:intl/intl.dart';
+import 'dart:math' as math;
 
-class SpeedometerPage extends StatefulWidget  {
+class SpeedometerPage extends StatefulWidget {
   const SpeedometerPage({Key? key}) : super(key: key);
 
   @override
@@ -12,50 +15,84 @@ class SpeedometerPage extends StatefulWidget  {
 class _SpeedometerPageState extends State<SpeedometerPage> {
   late List<SalesData> _chatData;
   late TooltipBehavior _tooltipBehavior;
+  late ChartSeriesController chartSeriesController;
+  late ZoomPanBehavior zoomPanBehavior;
+  late bool isPointerMoved;
 
   @override
-  void initState(){
+  void initState() {
     _chatData = getCharData();
-    _tooltipBehavior = TooltipBehavior(enable:true);
+    isPointerMoved = false;
+    zoomPanBehavior = ZoomPanBehavior(
+      enablePinching: true,
+      zoomMode: ZoomMode.x,
+      enablePanning: true,
+    );
+    _tooltipBehavior = TooltipBehavior(enable: true);
+    Timer.periodic(const Duration(seconds: 1), updateBro);
     super.initState();
   }
 
   @override
   Widget build(BuildContext context) {
-    return SafeArea(child: Scaffold(body: SfCartesianChart(
-      title: ChartTitle(text:'Speed of Vehical'),
-      legend: Legend(isVisible: true),
-      tooltipBehavior: _tooltipBehavior,
-      series:<ChartSeries>[
-        LineSeries<SalesData,double>(
-          name:'speed',
-          dataSource: _chatData,
-          xValueMapper: (SalesData sales, _) => sales.year,
-          yValueMapper: (SalesData sales ,_) => sales.sales,
-          dataLabelSettings: const DataLabelSettings(isVisible: true),
-        enableTooltip: true)
-      ],
-      primaryYAxis: NumericAxis(edgeLabelPlacement: EdgeLabelPlacement.shift),
-      primaryXAxis: NumericAxis(
-          labelFormat: '{value}M',
-          numberFormat: NumberFormat.simpleCurrency(decimalDigits: 0)),
-    ),
-    ));
+    return SafeArea(
+        child: Scaffold(
+            body: Padding(
+                padding: const EdgeInsets.all(20.0),
+                child: SfCartesianChart(
+                    onChartTouchInteractionMove:
+                        (ChartTouchInteractionArgs args) {
+                      isPointerMoved = true;
+                    },
+                    onChartTouchInteractionUp:
+                        (ChartTouchInteractionArgs args) {
+                      isPointerMoved = false;
+                    },
+                    zoomPanBehavior: zoomPanBehavior,
+                    title: ChartTitle(text: 'Speed of Vehical'),
+                    legend: Legend(isVisible: true),
+                    tooltipBehavior: _tooltipBehavior,
+                    enableAxisAnimation: true,
+                    series: <ChartSeries>[
+                      LineSeries<SalesData, double>(
+                          onRendererCreated:
+                              (ChartSeriesController controller) {
+                            chartSeriesController = controller;
+                          },
+                          name: 'speed',
+                          dataSource: _chatData,
+                          xValueMapper: (SalesData sales, _) => sales.year,
+                          yValueMapper: (SalesData sales, _) => sales.sales,
+                          dataLabelSettings:
+                              const DataLabelSettings(isVisible: true),
+                          enableTooltip: true)
+                    ],
+                    primaryYAxis: NumericAxis(
+                        edgeLabelPlacement: EdgeLabelPlacement.shift),
+                    primaryXAxis: NumericAxis(
+                        autoScrollingMode: AutoScrollingMode.end,
+                        autoScrollingDelta: 10,
+                        labelFormat: '{value}M',
+                        numberFormat:
+                            NumberFormat.simpleCurrency(decimalDigits: 0))))));
   }
-  List<SalesData>getCharData(){
-    final List<SalesData> chartData=[
-      SalesData(2017,25),
-      SalesData(2018,12),
-      SalesData(2019,24),
-      SalesData(2022,18),
-      SalesData(2021,30),
-    ];
+
+  double date = 2018;
+
+  void updateBro(Timer timer) {
+    _chatData.add(SalesData(date++, math.Random().nextDouble()));
+    chartSeriesController.updateDataSource(
+        addedDataIndex: _chatData.length - 1);
+  }
+
+  List<SalesData> getCharData() {
+    final List<SalesData> chartData = [SalesData(2017, 0.56)];
     return chartData;
   }
 }
 
-class SalesData{
-  SalesData(this.year,this.sales);
+class SalesData {
+  SalesData(this.year, this.sales);
   final double year;
   final double sales;
 }
